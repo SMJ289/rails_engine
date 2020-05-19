f8b5eb07d4283266a71316d8c1537dda5d9adb0a
Steven Meyers <sjjmeyers@gmail.com>
2020-05-19 15:12:16 -0600

    Add relationship data routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, except: [:new, :edit]
-      resources :items, except: [:new, :edit]
+      resources :merchants, except: [:new, :edit] do
+        get '/items', to: 'items_by_merchant#index'
+      end
+      resources :items, except: [:new, :edit] do
+        get '/merchant', to: 'merchant_by_item#show'
+      end
     end
   end
 end
