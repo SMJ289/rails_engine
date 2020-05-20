9248e602b9edd2fabcca18c854dd0f2d89eb9839
Steven Meyers <sjjmeyers@gmail.com>
2020-05-19 21:19:58 -0600

    Test single query find endpoints

---
+++ spec/requests/api/v1/merchant_find_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+describe 'Merchant API' do
+  it 'can find one merchant by name query parameter' do
+    merchant1 = Merchant.create(name: 'Steve World')
+    merchant2 = Merchant.create(name: 'Bob World')
+    merchant3 = Merchant.create(name: 'Sue World')
+
+    query = 'eve'
+    
+    get "/api/v1/merchants/find?name=#{query}"
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+  
+    expect(merchants.length).to eq(1)
+    expect(merchants[:data][:attributes][:name]).to eq(merchant1.name)
+
+  end
+
+  it 'can find one merchant by date query parameter' do
+    merchant1 = Merchant.create(name: 'Steve World')
+    merchant2 = Merchant.create(name: 'Bob World')
+    merchant3 = Merchant.create(name: 'Sue World')
+
+    query = 'eve'
+    
+    get "/api/v1/merchants/find?name=#{query}"
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+  
+    expect(merchants.length).to eq(1)
+    expect(merchants[:data][:attributes][:name]).to eq(merchant1.name)
+
+  end
+
+  it 'can find all merchants by name query parameter' do
+    merchant1 = Merchant.create(name: 'Steve World')
+    merchant2 = Merchant.create(name: 'Bob World')
+    merchant3 = Merchant.create(name: 'Sue World')
+    merchant4 = Merchant.create(name: 'Stan Land')
+
+    query = 'orld'
+    
+    get "/api/v1/merchants/find_all?name=#{query}"
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants[:data].length).to eq(3)
+  end
+
+end
