3f7b4f566437d8407d5005757eba8756612800bf
Steven Meyers <sjjmeyers@gmail.com>
2020-05-19 18:42:08 -0600

    add find by name method to merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,8 @@ class Merchant < ApplicationRecord
   validates_presence_of :name
   has_many :items
   has_many :invoices
+
+  def self.find_by_name(query)
+    find_by("lower(name) like ?", "%#{query.downcase}%")
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -9,4 +9,26 @@ RSpec.describe Merchant do
     it {should have_many :items}
     it {should have_many :invoices}
   end
+
+  describe 'class methods' do
+    it '.find_by_name()' do
+      merchant1 = Merchant.create(name: 'Steve World')
+      merchant2 = Merchant.create(name: 'Bob World')
+      merchant3 = Merchant.create(name: 'Sue World')
+
+      expect(Merchant.find_by_name('eve')).to eq(merchant1)
+      expect(Merchant.find_by_name('St')).to eq(merchant1)
+      expect(Merchant.find_by_name('ob')).to eq(merchant2)
+    end
+
+    it '.find_by_created_at()' do
+      merchant1 = Merchant.create(name: 'Steve World')
+      merchant2 = Merchant.create(name: 'Bob World')
+      merchant3 = Merchant.create(name: 'Sue World')
+
+      expect(Merchant.find_by_name('eve')).to eq(merchant1)
+      expect(Merchant.find_by_name('St')).to eq(merchant1)
+      expect(Merchant.find_by_name('ob')).to eq(merchant2)
+    end
+  end
 end
