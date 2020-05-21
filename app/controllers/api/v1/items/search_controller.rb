84eeb79117b03c6742ddfa40f0ea15b2afc84117
Steven Meyers <sjjmeyers@gmail.com>
2020-05-20 21:04:45 -0600

    Call new model method in ItemSearchController and test

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,22 +1,16 @@
 class Api::V1::Items::SearchController < ApplicationController
 
   def show
-    if params['name'].present?
-      render json: ItemSerializer.new(Item.find_by_name(params["name"]))
-    elsif params['description'].present?
-      render json: ItemSerializer.new(Item.find_by_description(params["description"]))
-    elsif params['unit_price'].present?
-      render json: ItemSerializer.new(Item.find_by_price(params["unit_price"]))
-    end
+    render json: ItemSerializer.new(Item.find_by_params(item_params))
   end
   
   def index
-    if params['name'].present?
-      render json: ItemSerializer.new(Item.find_all_by_name(params["name"]))
-    elsif params['description'].present?
-      render json: ItemSerializer.new(Item.find_all_by_description(params["description"]))
-    elsif params['unit_price'].present?
-      render json: ItemSerializer.new(Item.find_all_by_price(params["unit_price"]))
-    end
+    render json: ItemSerializer.new(Item.find_all_by_params(item_params))
+  end
+  
+  private
+
+  def item_params
+    params.permit(:name, :description, :unit_price)
   end
 end
--- spec/requests/api/v1/item_find_spec.rb
+++ spec/requests/api/v1/item_find_spec.rb
@@ -12,10 +12,10 @@ describe 'Item API' do
 
     expect(response).to be_successful
 
-    items = JSON.parse(response.body, symbolize_names: true)
+    item_response = JSON.parse(response.body, symbolize_names: true)
   
-    expect(items.length).to eq(1)
-    expect(items[:data][:attributes][:name]).to eq(item1.name)
+    expect(item_response.length).to eq(1)
+    expect(item_response[:data][:attributes][:name]).to eq(item1.name)
 
   end
 
@@ -30,9 +30,9 @@ describe 'Item API' do
 
     expect(response).to be_successful
 
-    items = JSON.parse(response.body, symbolize_names: true)
+    item_response = JSON.parse(response.body, symbolize_names: true)
   
-    expect(items[:data].length).to eq(3)
+    expect(item_response[:data].length).to eq(3)
   end
 
   it 'can find one item by description query parameter' do
@@ -47,10 +47,10 @@ describe 'Item API' do
 
     expect(response).to be_successful
 
-    items = JSON.parse(response.body, symbolize_names: true)
+    item_response = JSON.parse(response.body, symbolize_names: true)
   
-    expect(items.length).to eq(1)
-    expect(items[:data][:attributes][:name]).to eq(item1.name)
+    expect(item_response.length).to eq(1)
+    expect(item_response[:data][:attributes][:name]).to eq(item1.name)
   end
 
   it 'can find all items by description query parameter' do
@@ -65,9 +65,9 @@ describe 'Item API' do
 
     expect(response).to be_successful
 
-    items = JSON.parse(response.body, symbolize_names: true)
+    item_response = JSON.parse(response.body, symbolize_names: true)
   
-    expect(items[:data].length).to eq(3)
+    expect(item_response[:data].length).to eq(3)
   end
 
   it 'can find one item by unit_price query parameter' do
@@ -81,10 +81,10 @@ describe 'Item API' do
 
     expect(response).to be_successful
 
-    items = JSON.parse(response.body, symbolize_names: true)
+    item_response = JSON.parse(response.body, symbolize_names: true)
   
-    expect(items.length).to eq(1)
-    expect(items[:data][:attributes][:name]).to eq(item1.name)
+    expect(item_response.length).to eq(1)
+    expect(item_response[:data][:attributes][:name]).to eq(item1.name)
 
   end
 
@@ -99,8 +99,137 @@ describe 'Item API' do
 
     expect(response).to be_successful
 
-    items = JSON.parse(response.body, symbolize_names: true)
+    item_response = JSON.parse(response.body, symbolize_names: true)
   
-    expect(items[:data].length).to eq(3)
+    expect(item_response[:data].length).to eq(3)
+  end
+
+  it 'can find one item by 2 queries when both parameters match' do
+    item1 = create(:item, name: 'Boat', description: "Floats on water.", unit_price: 10.85)
+    item2 = create(:item, name: 'Car', description: 'Has wheels.', unit_price: 7.55)
+    item3 = create(:item, name: 'Plane', description: 'Flies in the air.', unit_price: 212.75)
+    
+    name_query = 'boat'
+    description_query = 'water'
+
+    get "/api/v1/items/find?name=#{name_query}&description=#{description_query}"
+  
+    expect(response).to be_successful
+
+    item_response = JSON.parse(response.body, symbolize_names: true)
+    expect(item_response.length).to eq(1)
+    expect(item_response[:data][:attributes][:name]).to eq(item1.name)
+
+    name_query = 'boat'
+    description_query = 'wheels'
+
+    get "/api/v1/items/find?name=#{name_query}&description=#{description_query}"
+  
+    expect(response).to be_successful
+    item_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(item_response[:data]).to be_nil
+
+    price_query = 10.85
+    description_query = 'water'
+
+    get "/api/v1/items/find?price=#{price_query}&description=#{description_query}"
+  
+    expect(response).to be_successful
+
+    item_response = JSON.parse(response.body, symbolize_names: true)
+    expect(item_response.length).to eq(1)
+    expect(item_response[:data][:attributes][:name]).to eq(item1.name)
+
+    name_query = 'car'
+    price_query = 7.55
+
+    get "/api/v1/items/find?name=#{name_query}&price=#{price_query}"
+  
+    expect(response).to be_successful
+    item_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(item_response.length).to eq(1)
+    expect(item_response[:data][:attributes][:name]).to eq(item2.name)
+  end
+
+  it 'can find one item by 3 queries when all parameters match' do
+    item1 = create(:item, name: 'Boat', description: "Floats on water.", unit_price: 10.85)
+    item2 = create(:item, name: 'Car', description: 'Has wheels.', unit_price: 7.55)
+    item3 = create(:item, name: 'Plane', description: 'Flies in the air.', unit_price: 212.75)
+    
+    name_query = 'boat'
+    description_query = 'water'
+    price_query = 10.85
+
+    get "/api/v1/items/find?name=#{name_query}&description=#{description_query}&unit_price=#{price_query}"
+  
+    expect(response).to be_successful
+
+    item_response = JSON.parse(response.body, symbolize_names: true)
+    expect(item_response.length).to eq(1)
+    expect(item_response[:data][:attributes][:name]).to eq(item1.name)
+    
+    name_query = 'boat'
+    description_query = 'flies'
+    price_query = 10.85
+
+    get "/api/v1/items/find?name=#{name_query}&description=#{description_query}&unit_price=#{price_query}"
+  
+    expect(response).to be_successful
+
+    item_response = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(item_response[:data]).to be_nil
+  end
+
+  it 'can find all items by 2 queries when both parameters match' do
+    item1 = create(:item, name: 'Small Boat', description: "Floats on water.", unit_price: 10.85)
+    item2 = create(:item, name: 'Tiny Car', description: 'Has wheels.', unit_price: 7.55)
+    item3 = create(:item, name: 'Micro Plane', description: 'Flies in the air.', unit_price: 212.75)
+    
+    item4 = create(:item, name: 'Big Boat', description: "Floats on seawater.", unit_price: 10.85)
+    item5 = create(:item, name: 'Large Car', description: 'Has rubber wheels.', unit_price: 7.55)
+    item6 = create(:item, name: 'Giant Plane', description: 'Flies in the warm air.', unit_price: 212.75)
+    
+    name_query = 'boat'
+    description_query = 'water'
+
+    get "/api/v1/items/find_all?name=#{name_query}&description=#{description_query}"
+  
+    expect(response).to be_successful
+
+    item_response = JSON.parse(response.body, symbolize_names: true)
+    expect(item_response[:data].length).to eq(2)
+
+    name_query = 'boat'
+    description_query = 'wheels'
+
+    get "/api/v1/items/find_all?name=#{name_query}&description=#{description_query}"
+  
+    expect(response).to be_successful
+    item_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(item_response[:data]).to eq([])
+
+    price_query = 10.85
+    description_query = 'water'
+
+    get "/api/v1/items/find_all?price=#{price_query}&description=#{description_query}"
+  
+    expect(response).to be_successful
+
+    item_response = JSON.parse(response.body, symbolize_names: true)
+    expect(item_response[:data].length).to eq(2)
+
+    name_query = 'car'
+    price_query = 7.55
+
+    get "/api/v1/items/find_all?name=#{name_query}&price=#{price_query}"
+  
+    expect(response).to be_successful
+    item_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(item_response[:data].length).to eq(2)
   end
 end
